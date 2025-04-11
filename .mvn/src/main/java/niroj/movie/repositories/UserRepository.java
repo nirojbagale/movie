07b5a3d0b89c472e08b3414c594e317142a29fb6
nirojbagale <nirajbagale1618@gmail.com>
2025-04-10 23:48:13 -0400

    assignment 3

--- .mvn/src/main/java/niroj/movie/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package niroj.movie.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import niroj.movie.models.UserModel;
-import java.util.Optional;
-
-public interface UserRepository extends MongoRepository<UserModel, String> {
-    Optional<UserModel> findByEmail(String email);
-}
